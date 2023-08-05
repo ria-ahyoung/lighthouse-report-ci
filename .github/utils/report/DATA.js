b8874610cf73794d60e5f0cbb3e4add42dc1b98e
ria-ahyoung <ria.ahyoung@gmail.com>
2023-08-05 22:52:46 +0900

    chore: add `WEB_VITALS_METRICS` Data

---
+++ .github/utils/report/DATA.js
@@ -0,0 +1,10 @@
+export const WEB_VITALS_METRICS = [
+  "first-contentful-paint",
+  "first-meaningful-paint",
+  "speed-index",
+  "total-blocking-time",
+  "largest-contentful-paint",
+  "cumulative-layout-shift",
+];
+
+export const REPORT_FILE_PATH = 'lhci_reports/manifest.json';
