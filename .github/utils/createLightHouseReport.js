33a07de7cc42709900cc9c5dc7a2daaaddf9d170
ria-ahyoung <ria.ahyoung@gmail.com>
2023-08-06 14:02:41 +0900

    feat: add `Lighthouse Performance Report` logic

---
+++ .github/utils/createLightHouseReport.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+
+import { REPORT_FILE_PATH } from "./report/DATA.js";
+import { getWebVitalsReport, getAuditsReport } from "./report/index.js";
+
+export default async function createLightHouseReport() {
+  const results = JSON.parse(fs.readFileSync(REPORT_FILE_PATH));
+
+  console.log('### **Lighthouse Performance Report**')
+  console.log(`> Lighthouse 성능 측정 자동화 리포트 (측정 횟수 : \`${results.length}\`)\n`)
+
+  getAuditsReport(results)
+  getWebVitalsReport(results);
+}
+
+await createLightHouseReport();
