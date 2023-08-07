170bdf8483122134df7b178dce5d5eb395372acd
ria-ahyoung <ria.ahyoung@gmail.com>
2023-08-07 22:56:14 +0900

    feat: update `audits` report logics

--- .github/utils/report/getAuditsReport.js
+++ .github/utils/report/getAuditsReport.js
@@ -1,17 +1,23 @@
+import { IS_SHOW_DETAIL } from "./static.js";
 import getScoreBadge from "./getScoreBadge.js";
 import formatCategory from "../format/formatCategory.js";
 
 export default function getAuditsReport(data) {
   const averageObj = {};
-  const AuditList = Object.keys(data[0].summary);
+  const auditKeys = Object.keys(data[0].summary);
 
-  AuditList.forEach((key) => {
+  auditKeys.forEach((key) => {
     const sum = data.reduce((acc, item) => acc + item.summary[key], 0);
     averageObj[key] = (sum / data.length).toFixed(2);
   });
 
+  getAverageContent(averageObj);
+  IS_SHOW_DETAIL && getTotalContent(averageObj, auditKeys, data);
+}
+
+function getAverageContent(averageObj) {
   const AuditsResult = [
-    "| Scale | Category | Score |",
+    "| Scale | Category | `Average` |",
     "| --- | --- | --- |",
     ...getTableContent(averageObj),
   ].join("\n");
@@ -20,11 +26,28 @@ export default function getAuditsReport(data) {
   console.log(AuditsResult + '\n');
 }
 
+function getTotalContent(averageObj, auditKeys, data) {
+  const detailResult = auditKeys.map(category => {
+    const scores = data.map(({ summary }) => Math.round(summary[category] * 100));
+    const averageBadge = getScoreBadge(averageObj[category] * 100);
+    return `| ${averageBadge} | **${formatCategory(category)}** | ${scores.join(' | ')} |`;
+  }) 
+  const scoreHeaders = data.map((_, index) => `Score ${index + 1}`)
+  const tableDivider = data.map((_) => ` ---- `)
+
+  console.log('<details><summary>Detail Results</summary>\n');
+  console.log(`| Scale | Category | ${scoreHeaders.join(' | ')} |`);
+  console.log(`| ---- | ---- | ${tableDivider.join(' | ')} |`);
+  console.log(detailResult.join('\n') + '\n');
+  console.log('</details>\n\n')
+}
+
+
 function getTableContent(averageObj) {
   return Object.entries(averageObj).map(([category, scoreValue]) => {
     const scorePercent = Math.round(scoreValue * 100);
     const scoreBadge = getScoreBadge(scorePercent);
     const formattedCategory = formatCategory(category);
-    return `| ${scoreBadge} | ${formattedCategory} | ${scorePercent} |`;
+    return `| ${scoreBadge} | **${formattedCategory}** | ${scorePercent} |`;
   });
 }
